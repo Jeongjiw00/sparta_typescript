c693ba274536acd0630fa2cdd9e289edc8efffc3
Jeongjiw00 <wldn0229@naver.com>
2023-01-21 18:54:22 +0900

    3일차 pokemon feat:fetchData

--- 3rd-day-pokemon/src/app.ts
+++ 3rd-day-pokemon/src/app.ts
@@ -9,3 +9,30 @@ interface IPokemon {
   image: string;
   type: string;
 }
+
+// 가져올 데이터 수만큼 getPokemon함수를 실행시켜줄 함수생성
+const fetchData = (): void => {
+  for (let i = 1; i <= pokemons; i++) {
+    getPokemon(i);
+  }
+};
+
+// 비동기처리위해서 async함수를 사용 => return값에 promise사용
+const getPokemon = async (id: number): Promise<void> => {
+  const data: Response = await fetch(`http://pokeapi.co/api/v2/pokemon/${id}`);
+  // json형태로 한 번 더 가공필, 데이터가 너무 많아서 any타입사용
+  const pokemon: any = await data.json();
+  const pokemonType: string = pokemon.types
+    // 타입이 여러개인 경우가 있어서
+    .map((poke: any) => poke.type.name)
+    .join(", ");
+
+  const transformedPokemon: IPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    image: `${pokemon.sprites.front_default}`,
+    type: pokemonType,
+  };
+
+  showPokemon(transformedPokemon);
+};
