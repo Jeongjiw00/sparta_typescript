1bfa61d3bf6ca06c2f86ede00db010b279bdf4ac
Jeongjiw00 <wldn0229@naver.com>
2023-01-23 19:19:46 +0900

    5일차 기본설정

---
+++ 5th/src/index.ts
@@ -0,0 +1,26 @@
+// Required External Modules - 모듈
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+
+// App Variables - 앱 변수 선언
+//포트가 없으면 프로그램 종료
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const app = express();
+
+// App Configuration - JSON 형식으로 데이터를 전달받기위해서
+app.use(express.json());
+
+// Server Activation - 서버 활성화
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
