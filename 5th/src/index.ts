7ab543034e28a8df6da7fd8df817c51a7ba0ce4d
Jeongjiw00 <wldn0229@naver.com>
2023-01-23 21:36:48 +0900

    5일차 index.ts에 router추가

--- 5th/src/index.ts
+++ 5th/src/index.ts
@@ -1,6 +1,7 @@
 // Required External Modules - 모듈
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const app = express();
 
 // App Configuration - JSON 형식으로 데이터를 전달받기위해서
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
 
 // Server Activation - 서버 활성화
 app.get("/", (req: Request, res: Response) => {
