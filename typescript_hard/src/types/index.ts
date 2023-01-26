e9eb66ad302a490fc6646dbbfb5f1a9eda08279c
Jeongjiw00 <wldn0229@naver.com>
2023-01-27 02:12:45 +0900

    type추가

--- typescript_hard/src/types/index.ts
+++ typescript_hard/src/types/index.ts
@@ -3,3 +3,22 @@ export enum Color {
   Green = "Green",
   Blue = "Blue",
 }
+
+export enum Direction {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+// interface 키워드로 타입 선언하기
+export interface IThingsInLife {
+  colorOfPen: Color;
+  keyboardArrow: Direction;
+}
+
+// // type 키워드로 타입 선언하기
+// type TThingsInLifeType = {
+//   colorOfPen: Color;
+//   keyboardArrow: Direction;
+// };
