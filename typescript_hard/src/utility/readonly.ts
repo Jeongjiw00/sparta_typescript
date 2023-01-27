cb0f0a7b1a16c878d5c0ed8ea9af6d171ccb95c5
Jeongjiw00 <wldn0229@naver.com>
2023-01-27 21:00:14 +0900

    utility types readonly

---
+++ typescript_hard/src/utility/readonly.ts
@@ -0,0 +1,13 @@
+interface BankAccount {
+  accountNumber: string;
+  balance: bigint;
+}
+
+const myAccount: Readonly<BankAccount> = {
+  accountNumber: "1234",
+  balance: BigInt(Number.MAX_SAFE_INTEGER),
+};
+
+// 컴파일되지 않습니다
+// myAccount.accountNumber = "123"; // ❌ Cannot assign to 'accountNumber' because it is a read-only property.
+// myAccount.balance = BigInt(Number.MIN_VALUE); // ❌ Cannot assign to 'balance' because it is a read-only property.
