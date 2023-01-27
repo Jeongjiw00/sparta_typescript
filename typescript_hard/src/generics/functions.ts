3a87693c1b008c3f75f5802aa2f9017b1c90bf66
Jeongjiw00 <wldn0229@naver.com>
2023-01-27 12:19:21 +0900

    generic types function

---
+++ typescript_hard/src/generics/functions.ts
@@ -0,0 +1,15 @@
+type User = {
+  email: string;
+  name: string;
+};
+
+function getData<T>(data: T): T {
+  return data;
+}
+
+// 에러 없이 콘솔로그 되는 유효한 호출
+console.log(getData<string>("string data"));
+console.log(getData<number>(1234));
+console.log(getData<User>({ email: "email@email.com", name: "katie" }));
+console.log(getData<string[]>(["string", "data"]));
+console.log(getData<string[]>([])); // 빈 배열도 유효한 인자입니다!
