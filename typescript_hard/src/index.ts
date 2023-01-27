04284244e2dc3a3bfcdbb5a7c827b7a85bc5e46b
Jeongjiw00 <wldn0229@naver.com>
2023-01-28 00:30:25 +0900

    Object.entries

--- typescript_hard/src/index.ts
+++ typescript_hard/src/index.ts
@@ -1,12 +1,21 @@
 // Types
-import type { Order } from "./types";
+// import type { Order } from "./types";
+
+// Object.entries
+// 1. Order의 orderStatus의 타입을 [string, Status]로 변경해준다.
+export interface Order {
+  buyer: string;
+  orderStatus: [string, Status];
+}
+
 // Enums
 import { Status } from "./types";
 
-// const ordersE: Order[] = Object.entries(Status).map((status, index) => {
-//   return {
-//     buyer: `buyer #${index}`,
-//     orderStatus: status,
-//   };
-// });
-// console.log(ordersE);
+const ordersE: Order[] = Object.entries(Status).map((status, index) => {
+  return {
+    buyer: `buyer #${index}`,
+    orderStatus: status,
+  };
+});
+
+console.log(ordersE);
