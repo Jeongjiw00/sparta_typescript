bf4c8245a5af209d7566764120c4b07821abad26
Jeongjiw00 <wldn0229@naver.com>
2023-01-27 18:49:18 +0900

    generic types class

---
+++ typescript_hard/src/generics/classes.ts
@@ -0,0 +1,44 @@
+interface IStack<T> {
+  push(item: T): void;
+  pop(): T | undefined;
+  peek(): T | undefined;
+  size(): number;
+}
+
+class Stack<T> implements IStack<T> {
+  private storage: T[] = [];
+
+  constructor(private capacity = 4) {}
+  // constructor : const stack = new Stack<string>(); 을 했을때 실행됨
+
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("stack is full");
+    }
+
+    this.storage.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
+
+  peek(): T | undefined {
+    return this.storage[this.size() - 1];
+  }
+
+  size(): number {
+    return this.storage.length;
+  }
+}
+
+const numberStack = new Stack<number>();
+numberStack.push(1);
+numberStack.push(2);
+numberStack.push(3);
+numberStack.push(100);
+
+console.log(numberStack.peek()); // 100
+console.log(numberStack.size()); // 4
+
+numberStack.push(101); // ❌ Error: stack is full
